9df44ace1b391c487255991051146766ea26a0be
Abdullah Tariq <a.tariq12382@gmail.com>
2021-11-16 21:35:26 +0500

    main

---
+++ models/Posts.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+const PostSchema= new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    contactNo:{
+        type:String,
+        required:true,
+    },
+    address:{
+        type:String,
+        required:true,
+    }
+});
+
+module.exports = mongoose.model('Posts', PostSchema);
---
+++ server.js
@@ -0,0 +1,42 @@
+//rest-api-tutorial
+//username->mahrukh
+//password->mahrukh123
+//link->mongodb+srv://mahrukh:<password>@cluster0.v3lph.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+//poroject0
+//username->f19_0103
+//password->Itj0CKvQrRpyfR68
+//link->mongodb+srv://f19_0103:Itj0CKvQrRpyfR68@cluster0.ngmmd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+const express = require('express');
+const mongoose = require('mongoose');
+const {MONGO_URI} = require('./config');
+
+//routes
+const postsRoutes = require('./routes/api/posts');
+
+const app = express();
+
+/*app.get('/', (req,res)=>{
+
+    res.send("hello from node");
+});*/
+
+//bodyParser middleware
+app.use(express.json());
+
+//connect to mongo db
+mongoose.connect(MONGO_URI,{
+    useNewUrlParser:true,
+    useUnifiedTopology: true,
+})
+
+    .then(()=> console.log('mongo db connected'))
+    .catch(err => console.log(err))
+
+
+//user routes
+app.use('/api/posts', postsRoutes);
+
+const port = 3000;
+
+app.listen(port,()=> console.log(`server is running at port ${port}.`));
