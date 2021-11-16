6bd1f5a33fb7fde0b1cf87468976d1663d9d3efd
Abdullah Tariq <a.tariq12382@gmail.com>
2021-11-16 21:40:17 +0500

    Updated mongo URI

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MONGO_URI: 'mongodb://f19_0103:Itj0CKvQrRpyfR68@cluster0-shard-00-00.ngmmd.mongodb.net:27017,cluster0-shard-00-01.ngmmd.mongodb.net:27017,cluster0-shard-00-02.ngmmd.mongodb.net:27017/mahrukhdb?ssl=true&replicaSet=atlas-lsogr4-shard-0&authSource=admin&retryWrites=true&w=majority'
+    MONGO_URI: 'mongodb://localhost:27017/BRDB'
 }
