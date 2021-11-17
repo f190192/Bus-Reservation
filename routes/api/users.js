39af52877988745b3612194de9246026c168b9a6
Abdullah Tariq <f190192@cfd.nu.edu.pk>
2021-11-17 16:13:51 +0500

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 // Load User Controller
 const userController = require('../../controllers/user.controllers')
 // Register
-//router.post('/register', userController.Register_User);
+router.post('/register', userController.Register_User);
 
 
 module.exports = router;
