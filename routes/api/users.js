a324ddbd044bbbd1710ed4c028742c8c32c6dbf6
Abdullah Tariq <f190192@cfd.nu.edu.pk>
2021-11-17 19:55:11 +0500

    main

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,14 @@ const router = express.Router();
 // Load User Controller
 const userController = require('../../controllers/user.controllers')
 // Register
+const { forwardAuthenticated } = require('../../config/auth');
+
+//Register Routes
+// Login Page
+router.get('/login', forwardAuthenticated, userController.login);
+// Register Page
+router.get('/register', forwardAuthenticated, userController.register);
+
 router.post('/register', userController.Register_User);
 
 
