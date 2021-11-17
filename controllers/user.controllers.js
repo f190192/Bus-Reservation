ef1bc6ad631c519855ecc44b64fcb694116a7a35
Abdullah Tariq <f190192@cfd.nu.edu.pk>
2021-11-17 12:00:28 +0500

    Update user.controllers.js

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,7 +1,73 @@
 const bcrypt = require("bcryptjs");
 const passport = require("passport");
 
-const user = require("../models/Posts");
+const User = require("../models/Posts");
 
-exports.login = (req, res) => res.render("register", {layout: "layouts/layout"}); //in place of second layout we will replace it with name of the ejs file which will we create later.
+exports.login = (req, res) => res.render("login", {layout: "layouts/layout"}); //layout is an ejs file in which we will write html of components which will be common in all pages.
+// "register" will be the name of the ejs file containing the view for register.
 
+exports.register = (req, res) => res.render("register", {layout: "layouts/layout"});
+
+exports.Register_User = (req, res) => {
+    const { name, email, password, contactno, address } = req.body;
+    let error = [];
+
+    if(!name || !email || !password || !contactno || !address)
+    {
+        error.push({ msg: "Please Enter Data in all the fields." });
+    }
+
+    if(password.length < 6)
+    {
+        error.push({ msg: "Length of the Password must be atleast 6 characters." });
+    }
+
+    if(error.length > 0)
+    {
+        res.render("register",{ error, name, email, password, contactno, address });
+    }
+    else
+    {
+        User.findOne({ email: email }).then(user => {
+            if(user)
+            {
+                error.push({ msg: "Email already exists" });
+                res.render("register",{ error, name, email, password, contactno, address });
+            }
+            else
+            {
+                const newUser = new User({ name, email, password, contactno, address });
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if(err) throw err;
+                        nwUser.password = hash;
+                        newUser
+                        .save()
+                        .then(user => {
+                            req.flash(
+                                "success_msg",
+                                "You have been registered, now you can login."
+                            );
+                            res.redirect("/users/login");
+                        })
+                        .catch(err => console.log(err));
+                    });
+                });
+            }
+        });
+    }
+};
+
+exports.Login_User = (req, res, next) => {
+    passport.authenticate("local", {
+        successRedirect: "/dashboard",
+        failureRedirect: "/users/login",
+        failureFlash: true
+    })(req, res, next);
+};
+
+exports.logout = (req, res) => {
+    req.logout();
+    req.flash("success_msg", "You are logged out");
+    res.redirect("/users/login");
+};
